96da2ab70816da85a0fd1101d26a3b9d88de8fa7
Konstantina <kwn.souv@gmail.com>
2025-02-20 22:17:49 +0200

    generateb CROSS and MAYO certs, fix nginx config, add percentiles with numpy

--- tls_timer.py
+++ tls_timer.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import csv
+import numpy as np
 
 
 # Define the command
@@ -17,13 +18,14 @@ def run_sclient_command(port, certificate, kem_algorithm):
     result = subprocess.run(command, text=True, timeout=10, input="", stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
     return result
 
-def run_qdisk_command(ns_name, ns_veth, pkt_loss):
+def run_qdisk_command(ns_name, ns_veth, pkt_loss, delay_ms):
     command = [
         'ip', 'netns', 'exec', ns_name,
         'tc', 'qdisc', 'change',
         'dev', ns_veth, 'root', 'netem',
         'limit', '1000',
         'loss', '{0}%'.format(pkt_loss),
+        'delay', delay_ms,
         'rate', '1000mbit'
     ]
 
@@ -35,31 +37,31 @@ def run_qdisk_command(ns_name, ns_veth, pkt_loss):
 # Signatures: DILITHIUM, CROSS, MAYO
 kem_algorithms = [
     "kyber512", 
-    # "p256_kyber512", 
-    # "x25519_kyber512", 
-    # "kyber768", 
-    # "p384_kyber768", 
-    # "x448_kyber768", 
-    # "x25519_kyber768", 
-    # "p256_kyber768", 
-    # "kyber1024", 
-    # "p521_kyber1024",
-    # "hqc128", 
-    # "p256_hqc128", 
-    # "x25519_hqc128", 
-    # "hqc192", 
-    # "p384_hqc192", 
-    # "x448_hqc192", 
-    # "hqc256", 
-    # "p521_hqc256",
-    # "bikel1", 
-    # "p256_bikel1", 
-    # "x25519_bikel1", 
-    # "bikel3", 
-    # "p384_bikel3", 
-    # "x448_bikel3", 
-    # "bikel5", 
-    # "p521_bikel5"
+    "p256_kyber512", 
+    "x25519_kyber512", 
+    "kyber768", 
+    "p384_kyber768", 
+    "x448_kyber768", 
+    "x25519_kyber768", 
+    "p256_kyber768", 
+    "kyber1024", 
+    "p521_kyber1024",
+    "hqc128", 
+    "p256_hqc128", 
+    "x25519_hqc128", 
+    "hqc192", 
+    "p384_hqc192", 
+    "x448_hqc192", 
+    "hqc256", 
+    "p521_hqc256",
+    "bikel1", 
+    "p256_bikel1", 
+    "x25519_bikel1", 
+    "bikel3", 
+    "p384_bikel3", 
+    "x448_bikel3", 
+    "bikel5", 
+    "p521_bikel5"
 ]
 
 certificates = [
@@ -68,44 +70,52 @@ certificates = [
         "certificate_file": "/etc/nginx/certs/dilithium3_srv.crt",
         "algorithm": "dilithium3"
     },
-    # {
-    #     "port": 4432,
-    #     "certificate_file": "/etc/nginx/certs/mayo3_srv.crt",
-    #     "algorithm": "mayo3", 
-    # },
-    # {
-    #     "port": 4433,
-    #     "certificate_file": "/etc/nginx/certs/CROSSrsdp128balanced_srv.crt",
-    #     "algorithm": "CROSSrsdp128balanced"
-    # }
+    {
+        "port": 4433,
+        "certificate_file": "/etc/nginx/certs/mayo3_srv.crt",
+        "algorithm": "mayo3", 
+    },
+    {
+        "port": 4432,
+        "certificate_file": "/etc/nginx/certs/CROSSrsdp128balanced_srv.crt",
+        "algorithm": "CROSSrsdp128balanced"
+    }
 ]
 
-iterations = 1000
+iterations = 300
 
 if __name__ == '__main__':
 
     for certificate in certificates: 
         port = certificate["port"]
         certificate_file = certificate["certificate_file"]
+        delay_ms = '0ms'
 
-        print("now running using certificate: ", certificate["algorithm"])
-        with open(f'data/{certificate["algorithm"]}.csv','w') as out:
+        print("now running using certificate: ", certificate["algorithm"], "with delay: ", delay_ms)
+        with open(f'data/{certificate["algorithm"]}_50th.csv','w') as out1, open(f'data/{certificate["algorithm"]}_95th.csv','w') as out2:
             fieldnames = [' '] + kem_algorithms
-            print(fieldnames)
-            writer = csv.DictWriter(out, delimiter=";", fieldnames=fieldnames)
-            writer.writeheader()
+            writer1 = csv.DictWriter(out1, delimiter=";", fieldnames=fieldnames)
+            writer1.writeheader()
 
-            for pkt_loss in [0, 0.1, 0.5, 1, 1.5, 2, 2.5, 3]:
+            writer2 = csv.DictWriter(out2, delimiter=";", fieldnames=fieldnames)
+            writer2.writeheader()
+
+            for pkt_loss in [0, 0.1, 0.5, 1, 1.5, 2, 2.5, 3, 4, 6, 8, 10, 12, 14, 16, 18, 20]:
                 # insert packet loss
-                run_qdisk_command("srv_ns", "srv_ve", pkt_loss)
-                run_qdisk_command("cli_ns", "cli_ve", pkt_loss)
+                run_qdisk_command("srv_ns", "srv_ve", pkt_loss, delay_ms)
+                run_qdisk_command("cli_ns", "cli_ve", pkt_loss, delay_ms)
 
-                row = {
+                row_50 = {
                     " ": pkt_loss
                 }
 
+                row_95 = {
+                    " ": pkt_loss
+                }
+
+
                 for kem in kem_algorithms:
-                    total_time = 0
+                    total_times = []
 
                     for i in range (0, iterations):
                         start_time = time.time()
@@ -113,7 +123,7 @@ if __name__ == '__main__':
                             result = run_sclient_command(port, certificate_file, kem)
                             end_time = time.time()
 
-                            total_time += end_time - start_time
+                            total_times.append(end_time - start_time)
 
                             if result.stderr:
                                 print("Error Output:", result.stderr)
@@ -122,10 +132,14 @@ if __name__ == '__main__':
                             print("Unexpected error:", str(e))
                     
                     # Print average execution time
-                    print(f"KEM: {kem} - Execution Time: {total_time/iterations:.3f} seconds")
-                    row[kem] = total_time/iterations
+                    percentile_50 = np.percentile(np.array(total_times), 50)
+                    percentile_95 = np.percentile(np.array(total_times), 95)
+                    print(f"KEM: {kem} - packet loss {pkt_loss} - delay {delay_ms} - Execution Time: 50th {percentile_50:.3f} 95th {percentile_95:.3f} seconds")
+                    row_50[kem] = percentile_50
+                    row_95[kem] = percentile_95
                     # writer.writerow({'kem': kem, 'pkt_loss': pkt_loss, 'time': total_time/iterations})
                 
-                writer.writerow(row)
+                writer1.writerow(row_50)
+                writer2.writerow(row_95)
 
 
